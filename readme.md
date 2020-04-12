b8b3229c10be587081e516908ce3f485acdc54d7
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-04-11 23:30:51 -0400

    updated index, script and css

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 homework 6
+
+Homework was to create a weather dashboard. A user would be able to search and city and current weather and five day forcast including City name, date, weather icon, temp, humidity, Wind Speed and UV index. UV index was suppose to give a severity warning. The city and information was to be saved to local storage so user could click and retrieve information.
+
+I was able to provided city search and some aspects of weather conditions in five day forecast. I was unable to retrieve UV index, couldn't figured out how to manipulate the city name into lat/long and get from API. I also kept getting an undefined error message when trying to get the weather icons. I also feel like I probably should have used a loop somehow. 
--- script.js
+++ script.js
@@ -9,8 +9,8 @@ $("#citySearch").on("click", function(event){
 
     
 
-    console.log(cityName);
-    console.log(queryURL);
+   //  console.log(cityName);
+   //  console.log(queryURL);
     
 
     $.ajax({
@@ -20,7 +20,6 @@ $("#citySearch").on("click", function(event){
     
     $(".city").html("<h2>" + response.name + " " + "Current Weather</h2>");
     $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".temp").text("Temperature: " + response.main.temp);
         $(".humidity").text("Humidity: " + response.main.humidity);
         var convertedTemp = (response.main.temp - 273.15) * 1.80 + 32;
         $(".convertedTemp").text("Temperature (F) " + convertedTemp.toFixed(2));
@@ -37,17 +36,53 @@ $("#citySearch").on("click", function(event){
   }).then(function(response){
           console.log(response);
 
-     var dayOneTemp = $("<p>").text("Temp:" + response.list[0].main.temp)
-     var dayOneHumidity = $("<p>").text("Humidity:" + response.list[0].main.humidity)
-     var dayTwoHumidity = $("#dayTwo").text("Humidity:" + response.list[1].main.humidity)
-     var dayThreeHumidity = $("#dayThree").text("Humidity:" + response.list[2].main.humidity)
-     var dayFourHumidity = $("#dayFour").text("Humidity:" + response.list[3].main.humidity)
-     var dayFiveHumidity = $("#dayFive").text("Humidity:" + response.list[4].main.humidity)
+   var convertedTemp2 = (response.list[0].main.temp -273.15) * 1.80 + 32;
+   var convertedTemp3 = (response.list[1].main.temp -273.15) * 1.80 + 32;
+   var convertedTemp4 = (response.list[2].main.temp -273.15) * 1.80 + 32;
+   var convertedTemp5 = (response.list[3].main.temp -273.15) * 1.80 + 32;
+   var convertedTemp6 = (response.list[4].main.temp -273.15) * 1.80 + 32;
+
+   
+   var dayOneTemp = $("<p>").text("Temp(F):" + convertedTemp2.toFixed(1));
+   var dayTwoTemp = $("<p>").text("Temp(F):" + convertedTemp3.toFixed(1));
+   var dayThreeTemp = $("<p>").text("Temp(F):" + convertedTemp4.toFixed(1));
+   var dayFourTemp = $("<p>").text("Temp(F):" + convertedTemp5.toFixed(1));
+   var dayFiveTemp = $("<p>").text("Temp(F):" + convertedTemp6.toFixed(1));
+
+     
+    var dayOneHumidity = $("<p>").text("Humidity:" + response.list[0].main.humidity);
+     var dayTwoHumidity = $("<p>").text("Humidity:" + response.list[1].main.humidity);
+     var dayThreeHumidity = $("<p>").text("Humidity:" + response.list[2].main.humidity);
+     var dayFourHumidity = $("<p>").text("Humidity:" + response.list[3].main.humidity);
+     var dayFiveHumidity = $("<p>").text("Humidity:" + response.list[4].main.humidity);
+
+     var dayOneWind = $("<p>").text("Wind" + response.list[0].wind.speed);
+     var dayTwoWind = $("<p>").text("Wind" + response.list[1].wind.speed);
+     var dayThreeWind = $("<p>").text("Wind" + response.list[2].wind.speed);
+     var dayFourWind = $("<p>").text("Wind" + response.list[3].wind.speed);
+     var dayFiveWind = $("<p>").text("Wind" + response.list[4].wind.speed);
 
 
-     console.log(dayOneTemp)
-     $("#dayOne").append(dayOneHumidity);
-     $("dayOne").prepend(dayOneTemp);
+     $("#dayOne").append(dayOneTemp, dayOneHumidity, dayOneWind);
+     $("#dayTwo").append(dayTwoTemp, dayTwoHumidity, dayTwoWind);
+     $("#dayThree").append(dayThreeTemp, dayThreeHumidity, dayThreeWind);
+     $("#dayFour").append(dayFourTemp, dayFourHumidity, dayFourWind);
+     $("#dayFive").append(dayFiveTemp, dayFiveHumidity, dayFiveWind);
+     $("#weatherBox").append("<h5>" + "Five Day" + "</h5>");
+     
+
+
+
+//  Tried to get the weather Icons, but kept getting undefined message in the console
+//  var cdwCode = (response.list.weather[0].icon)    
+// var weatherIcon = " http://openweathermap.org/img/wn/" + cdwCode +  "@2x.png"
+
+// var dayOneImage = $("<img>").attr("scr", weatherIcon);
+// console.log(dayOneImage);
+
+
+
+
      
 
  
